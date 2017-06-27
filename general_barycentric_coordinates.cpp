481ce566ba443504c643babec391139b45589d87
Songrun <songruner@gmail.com>
2017-06-27 18:21:56 -0400

    local optimization works well.

--- general_barycentric_coordinates.cpp
+++ general_barycentric_coordinates.cpp
@@ -2,9 +2,10 @@
 #include <igl/read_triangle_mesh.h>
 #include <igl/list_to_matrix.h>
 #include <igl/readDMAT.h>
+#include <igl/writeDMAT.h>
 #include <igl/REDRUM.h>
 #include <igl/active_set.h>
-// #include <igl/mosek/mosek_quadprog.h>
+#include <igl/mosek/mosek_quadprog.h>
 
 #include <string>
 #include <vector>
@@ -22,6 +23,12 @@ namespace {
 		Eigen::MatrixXd V;	// #Vx3
 		Eigen::MatrixXi F;	// #Fx3
 		Eigen::MatrixXd T;	// #Hx12
+	};
+
+	enum SolverType{
+		mosek=0,
+		active_set,
+		eiquadprog
 	};
 
 	template<typename TimeT = std::chrono::milliseconds>
@@ -71,7 +78,7 @@ void solve_weights(
 	const std::vector<Pose> & poses, 
 	const Eigen::MatrixXd & W,
 	Eigen::MatrixXd & nW,
-	bool use_sparse_solver = false)
+	SolverType solver_type)
 {
 	using namespace std;
 	using namespace Eigen;
@@ -92,16 +99,19 @@ void solve_weights(
 	igl::cotmatrix(poses[0].V,poses[0].F,L);
 	
 	L = -L;
-	Eigen::SparseMatrix<double> A(n*h, n*h);
-	A.reserve(L.nonZeros()*h);
+
+	std::vector<Tr> tripletList;
+	tripletList.reserve(L.nonZeros()*h);
 	for(int i=0; i<h; i++) {
 		for (int k=0; k<L.outerSize(); ++k) {
 			for (SparseMatrix<double>::InnerIterator it(L,k); it; ++it)
 			{
-				A.insert(it.row()+n*i,it.col()+n*i) = it.value();
+				tripletList.push_back(Tr(it.row()+n*i,it.col()+n*i,it.value()));
 			}
 		}
 	}
+	Eigen::SparseMatrix<double> A(n*h, n*h);
+	A.setFromTriplets(tripletList.begin(), tripletList.end());
 
 	VectorXd B(n*h);
 	B.setZero();
@@ -110,7 +120,7 @@ void solve_weights(
 	cout << "Sparse A: " << A.nonZeros() << " " << A.rows() << " " << A.cols() << endl;
 
 	// add soft constraints TE.X + te0 = 0, representing sum_i(w_ij*T_ij) = T_j
-	std::vector<Tr> tripletList;
+	tripletList.clear();
 	tripletList.reserve(12*h*n);
 	
 	VectorXd te0(n*12*p);
@@ -157,25 +167,52 @@ void solve_weights(
 	// solve
 	VectorXd X;
 	
-	if( use_sparse_solver ) {
+	string output_system = "../models/system.DMAT";
+	igl::writeDMAT(output_system, MatrixXd(A));
 	
-		VectorXd lx(n*h), ux(n*h);
-		lx.setConstant(0);
-		ux.setConstant(1);
+	if( solver_type == mosek ) {
+		VectorXd lx(n*h);
+		VectorXd ux(n*h);
+		lx.setZero();
+		ux.setOnes();
+		VectorXd lc, uc;
+		
+		// convert equality constraints to energy
+		MatrixXd EQ = Aeq*Aeq.transpose()*penalty_weight;
+// 		VectorXd eq0 = Aeq*Beq
+		
+	} 
+	else if( solver_type == active_set ) {
+	
+// 		VectorXd lx(n*h), ux(n*h);
+// 		lx.setConstant(0);
+// 		ux.setConstant(1);
+// 		Eigen::SparseMatrix<double> Aieq;
+// 		VectorXd Bieq;
+		
+		VectorXd lx, ux;
+		tripletList.clear();
+		tripletList.reserve(n*h);
+	
+		for(int i=0; i<n*h; i++) {
+			tripletList.push_back(Tr(i,i,1));
+		}
+		Eigen::SparseMatrix<double> Aieq(n*h,n*h);
+		Aieq.setFromTriplets(tripletList.begin(), tripletList.end());
+		VectorXd Bieq(n*h);
+		Bieq.setZero();
 		cout << "Inequality constraints built." << endl;
 	
 		VectorXi known;
 		VectorXd Y;
-		Eigen::SparseMatrix<double> Aieq;
-		VectorXd Bieq;
 		
 		igl::active_set_params params;
-		params.Auu_pd = true;
+		params.Auu_pd = false;
 		params.max_iter = 1;
 	
 		igl::active_set(A,B,known,Y,Aeq,Beq,Aieq,Bieq,lx,ux,params,X);
 	}
-	else {
+	else if( solver_type == eiquadprog ) {
 		// w_ij >= 0
 		tripletList.clear();
 		tripletList.reserve(n*h);
@@ -195,7 +232,17 @@ void solve_weights(
 		VectorXd ce0 = -Beq;
 		MatrixXd CI = MatrixXd(Aieq).transpose();
 		VectorXd ci0 = -Bieq;
+		
+		Eigen::LLT<Eigen::MatrixXd> lltOfG(G); // compute the Cholesky decomposition of A
+		if(lltOfG.info() == Eigen::NumericalIssue)
+		{
+			throw std::runtime_error("Possibly non positive definite matrix!");
+		} 
+		
 		solve_quadprog(G,g0,CE,ce0,CI,ci0,X);
+	} else {
+		cerr << "Unknown solver" << endl;
+		exit(-1);
 	}
 	cout << GREENGIN("Weight solved.") << endl;
 	
@@ -204,7 +251,144 @@ void solve_weights(
 	for(int i=0; i<h; i++) {
 		nW.col(i) = X.segment(i*n,n);
 	}
-	cout << nW << endl;
+}
+
+void solve_weights_locally(
+	const std::vector<Pose> & poses, 
+	const Eigen::MatrixXd & W,
+	Eigen::MatrixXd & nW)
+{
+	using namespace std;
+	using namespace Eigen;
+	
+	typedef Eigen::Triplet<double> Tr;
+	
+	const int p = poses.size();				// #poses
+	assert(p>=1);
+	const int n = poses[0].V.rows();		// #vertices
+	const int h = poses[0].T.rows();		// #handles
+	assert( W.cols() == h );
+	
+	// Laplacian
+	Eigen::SparseMatrix<double> L;
+	igl::cotmatrix(poses[0].V,poses[0].F,L);
+	
+	L = -L;
+// 	Eigen::SparseMatrix<double> A2(n*h, n*h);
+
+	std::vector<Tr> tripletList;
+	tripletList.reserve(L.nonZeros()*h);
+	
+	for(int i=0; i<h; i++) {
+		for (int k=0; k<L.outerSize(); ++k) {
+			for (SparseMatrix<double>::InnerIterator it(L,k); it; ++it)
+			{
+				int row = it.row();
+				int col = it.col();
+				tripletList.push_back(Tr(row+n*i,col+n*i,it.value()/L.coeffRef(row,row)));
+// 				tripletList.push_back(Tr(row+n*i,col+n*i,it.value()));
+			}
+		}
+	}
+	Eigen::SparseMatrix<double> LL(n*h,n*h);
+	LL.setFromTriplets(tripletList.begin(), tripletList.end());
+
+// 	cout << L << endl;
+	cout << "#vertices: " << n << endl;
+	cout << "#handles: " << h << endl;
+	cout << "#poses: " << p << endl;
+	
+	cout << "Sparse Laplacian system built." << endl;
+	cout << "Sparse L: " << L.nonZeros() << " " << L.rows() << " " << L.cols() << endl;
+	cout << "Sparse LL: " << LL.nonZeros() << " " << LL.rows() << " " << LL.cols() << endl;
+	
+	const int max_iter = 1000;
+	MatrixXd curr_W(n,h),prev_W(n,h);
+	VectorXd smooth_W(n*h);
+	curr_W.setConstant(1./h);
+
+// 	curr_W.setZero();
+// 	for(int i=0; i<n; i++) 	curr_W(i,i%h) = 1;
+	
+// 	curr_W.setRandom();
+// 	curr_W = (curr_W.array()+1.)/2;
+// 	for(int i=0; i<n; i++)	curr_W.row(i) /= curr_W.row(i).sum();
+	
+	const double scale = 0.25;
+	const double threshold = 1e-5;
+	
+	cout << "curr_W:" << endl;
+	cout << curr_W << endl;
+	
+	for(int k=0; k<max_iter; k++) {
+		
+		prev_W = curr_W;
+		for(int i=0; i<h; i++)	smooth_W.segment(i*n,n) = curr_W.col(i);
+		VectorXd step = LL*smooth_W;
+		cout << "iter #" << k << " step: " << step.norm() << endl;
+		
+		cout << step.rows()  << " " << smooth_W.rows() << endl;
+		smooth_W -= scale*step;
+		
+		for(int i=0; i<n; i++) {
+			// minimize w - curr_W
+			MatrixXd I(h,h);
+			I.setIdentity();
+			
+			VectorXd i0(h);
+			for(int j=0; j<h; j++)	i0(j) = -smooth_W(j*n+i);
+// 			i0 = -smooth_W.segment(i*h,h).transpose();
+			
+			// add soft constraints TE.X + te0 = 0, representing sum_i(w_ij*T_ij) = T_j
+			MatrixXd TE(12*p,h);
+	
+			VectorXd te0(12*p);
+			for(int j=0; j<p; j++) {
+				TE.block(j*12,0,12,h) = poses[j].T.transpose();
+				te0.segment(j*12,12) = -(W.row(i)*poses[j].T).transpose();
+			}
+	
+			const double penalty_weight = 1e6;
+			MatrixXd A = TE.transpose()*TE*penalty_weight;
+
+			VectorXd B(h);
+			B = TE.transpose()*te0*penalty_weight;
+			// equality constraints, representing sum_i(w_ij) = 1
+			MatrixXd Aeq(1,h);
+			Aeq.setOnes();
+			VectorXd Beq(1);
+			Beq << 1;
+			
+			// inequality constraints, w_ij >= 0
+			MatrixXd Aieq(h,h);
+			Aieq.setIdentity();
+			VectorXd Bieq(h);
+			Bieq.setZero();
+			
+			// solve one vertex
+			MatrixXd G = I+A;
+			VectorXd g0 = i0+B;
+			MatrixXd CE = Aeq.transpose();
+			VectorXd ce0 = -Beq;
+			MatrixXd CI = Aieq.transpose();
+			VectorXd ci0 = -Bieq;
+			VectorXd X;
+			solve_quadprog(G,g0,CE,ce0,CI,ci0,X);
+			
+			curr_W.row(i) = X.transpose();
+// 			if(X.array().maxCoeff() > 1 || X.array().minCoeff() < 0) cout << X.transpose() << endl;
+		}
+		
+// 		cout << curr_W << endl;
+		
+		double norm = (prev_W-curr_W).norm();
+		cout << "weight difference norm: " << norm << endl;
+		if(norm <= threshold) {
+			cout << "Stop optimization at " << k << " iterations" << endl;
+			break;
+		}
+	}
+	nW = curr_W;
 }
 
 int main(int argc, char* argv[]) {
@@ -244,14 +428,24 @@ int main(int argc, char* argv[]) {
 		poses.push_back(P);
 	}
 	
+	SolverType solver_type = eiquadprog;
+	
 	MatrixXd nW;
 	bool use_sparse_solver = true;
-	Measure<>::execution(solve_weights, poses, W, nW, use_sparse_solver);
+	Measure<>::execution(solve_weights, poses, W, nW, solver_type);
+// 	Measure<>::execution(solve_weights_locally, poses, W, nW);
+	cout << nW << endl;
 	double max_error = (nW-W).array().abs().maxCoeff();
 	cout << "max error: " << max_error << endl;
+	
+// 	double max_weight = (nW).array().abs().maxCoeff();
+// 	cout << "max weight: " << max_weight << endl;
 	if( max_error > 1e-4) {
-		cout << "Weight Diff:\n" << nW-W << endl;
+// 		cout << "Weight:\n" << nW << endl;
 	}
+	
+	string output_weight = pythonlike::os_path_split(weight_path).first+"-estimated.DMAT";
+	igl::writeDMAT(output_weight, nW);
 	
 	return 0;
 }
