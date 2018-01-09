612069712c7d68c43236c2bfb838a474ee14bc4b
Yotam Gingold <yotam@yotamgingold.com>
2018-01-09 14:55:33 -0500

    Scripts in directories

--- results_yotam/cube4pose1_flat_intersection_ground_truth_output/run.sh
+++ results_yotam/cube4pose1_flat_intersection_ground_truth_output/run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash -x
 
-OUTPUT_DIR=./results_yotam/cube4pose1_robust
+OUTPUT_DIR=.
+ROOT_DIR="../.."
 
-REST_POSE=./models/cube4/cube.obj
-OTHER_POSE_DIR=./models/cube4/poses-1
+REST_POSE="${ROOT_DIR}"/models/cube4/cube.obj
+OTHER_POSE_DIR="${ROOT_DIR}"/models/cube4/poses-1
 
 INITIAL_GUESS_ARGS="--svd_threshold 1e-15 --transformation_threshold 1e-4 --version 0"
 FLAT_INTERSECTION_ARGS="--energy biquadratic -GT "${OTHER_POSE_DIR}" --error True --handles 4 --fancy-init "${OUTPUT_DIR}"/local_subspace_recover.txt"
@@ -19,10 +20,10 @@ cp "${OTHER_POSE_DIR}"/*.DMAT "${OUTPUT_DIR}"/
 python3 -u simplex_hull.py "${OUTPUT_DIR}" ${SIMPLEX_HULL_ARGS} 2>&1 | tee "${OUTPUT_DIR}"/simplex_hull.out
 
 # Evaluate
-python -u compare.py "${REST_POSE}" "${OTHER_POSE_DIR}" "$(dirname "${REST_POSE}")"/"$(basename "${REST_POSE}" .obj)".DMAT "${OUTPUT_DIR}"/result.txt 2>&1 | tee "${OUTPUT_DIR}"/compare.out
+python -u "${ROOT_DIR}"/compare.py "${REST_POSE}" "${OTHER_POSE_DIR}" "$(dirname "${REST_POSE}")"/"$(basename "${REST_POSE}" .obj)".DMAT "${OUTPUT_DIR}"/result.txt 2>&1 | tee "${OUTPUT_DIR}"/compare.out
 
 # Verify each step
 
 ## Verify flat_intersection.py
 ### All at once:
-parallel python flat_intersection_apply_output.py "${REST_POSE}" '{}' '{.}.obj' ::: "${OUTPUT_DIR}"/*.DMAT
+parallel python "${ROOT_DIR}"/flat_intersection_apply_output.py "${REST_POSE}" '{}' '{.}.obj' ::: "${OUTPUT_DIR}"/*.DMAT
