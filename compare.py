bc9708c285e1aac6ff214cfdb669d13dfbbbc12d
Songrun <songruner@gmail.com>
2018-01-09 15:05:17 -0500

    new examples

--- compare.py
+++ compare.py
@@ -136,6 +136,7 @@ if __name__ == '__main__':
 	parser.add_argument( 'our_result', type=str, help='our results(txt).')
 	parser.add_argument( '--write-OBJ', '-W', type=bool, default=False, help='whether to save recovered and SSD poses.')
 	parser.add_argument( '--ssd_result', '--SSD', type=str, help='SSD results(txt).')
+	parser.add_argument( '--debug', '-D', type=bool, default=False, help='print out debug information.')
 	args = parser.parse_args()
 	
 	base_dir = args.pose_folder
@@ -166,49 +167,77 @@ if __name__ == '__main__':
 	assert( len(rev_bones_unordered) == N )
 	rev_bones = rev_bones_unordered.copy()
 	rev_w = rev_w_unordered.copy()
-		
-	for i, gt_vals in enumerate(gt_bones):
-		rev_dist = inf
-		rev_idx = -1
 	
-		for j in range(i, N):
-			if( np.linalg.norm( gt_vals - rev_bones_unordered[j] ) < rev_dist ):
-				rev_idx = j
-				rev_dist = np.linalg.norm( gt_vals - rev_bones_unordered[j] )
+	ssd_bones = np.zeros(rev_bones.shape)
+	ssd_w = np.zeros(rev_w.shape)
 		
-		rev_bones[i] = rev_bones_unordered[rev_idx]
-		rev_bones_unordered[rev_idx] = rev_bones_unordered[i]
-		
-		rev_w[i] = rev_w_unordered[rev_idx]
-		rev_w_unordered[rev_idx] = rev_w_unordered[i]
-		
+# 	for i, gt_vals in enumerate(gt_bones):
+# 		rev_dist = inf
+# 		rev_idx = -1
+# 	
+# 		for j in range(i, N):
+# 			if( np.linalg.norm( gt_vals - rev_bones_unordered[j] ) < rev_dist ):
+# 				rev_idx = j
+# 				rev_dist = np.linalg.norm( gt_vals - rev_bones_unordered[j] )
+# 		
+# 		rev_bones[i] = rev_bones_unordered[rev_idx]
+# 		rev_bones_unordered[rev_idx] = rev_bones_unordered[i]
+# 		
+# 		rev_w[i] = rev_w_unordered[rev_idx]
+# 		rev_w_unordered[rev_idx] = rev_w_unordered[i]
+# 				
+# 	if args.ssd_result is not None:
+# 		ssd_bones_unordered, ssd_w_unordered = format_loader.load_result(args.ssd_result)
+# 		assert( len(ssd_bones_unordered) == N )
+# 		ssd_w = ssd_w_unordered.copy()
+# 		ssd_bones = ssd_bones_unordered.copy()
+# 		
+# 		for i, gt_vals in enumerate(gt_bones):
+# 			ssd_dist = inf
+# 			ssd_idx = -1
+# 			for j in range(i, N):
+# 				if( np.linalg.norm( gt_vals - ssd_bones_unordered[j] ) < ssd_dist ):
+# 					ssd_idx = j
+# 					ssd_dist = np.linalg.norm( gt_vals - ssd_bones_unordered[j] )
+# 		
+# 			ssd_bones[i] = ssd_bones_unordered[ssd_idx]
+# 			ssd_bones_unordered[ssd_idx] = ssd_bones_unordered[i]
+# 		
+# 			ssd_w[i] = ssd_w_unordered[ssd_idx]
+# 			ssd_w_unordered[ssd_idx] = ssd_w_unordered[i]
+
+	B = gt_bones.shape[0]
+	rev_bones_match = np.zeros((B,B))
+	rev_bones_match=((gt_bones.reshape((B,1,-1))-rev_bones.reshape((1,B,-1)))**2).sum(-1)
+	row_ind, col_ind = scipy.optimize.linear_sum_assignment( rev_bones_match )
+	rev_bones = np.array([ rev_bones[i] for i in col_ind ])
+	rev_w = np.array([ rev_w[i] for i in col_ind ])
+	
+	if args.ssd_result is not None:
+		ssd_bones, ssd_w = format_loader.load_result(args.ssd_result)
+		ssd_bones_match=((gt_bones.reshape((B,1,-1))-ssd_bones.reshape((1,B,-1)))**2).sum(-1)
+		row_ind, col_ind = scipy.optimize.linear_sum_assignment( ssd_bones_match )
+		ssd_bones = np.array([ ssd_bones[i] for i in col_ind ])
+		ssd_w = np.array([ ssd_w[i] for i in col_ind ])
+				
 	## Adjust bones data to Pose-by-bone-by-transformation
 	gt_bones = np.swapaxes(gt_bones, 0, 1) 
 	rev_bones = np.swapaxes(rev_bones, 0, 1)
 	rev_vs = np.array([lbs_all(rest_vs, rev_bones_per_pose, rev_w.T) for rev_bones_per_pose in rev_bones ])
-		
+	ssd_vs = np.zeros(rev_vs.shape)
 	if args.ssd_result is not None:
-		ssd_bones_unordered, ssd_w_unordered = format_loader.load_result(args.ssd_result)
-		assert( len(ssd_bones_unordered) == N )
-		ssd_w = ssd_w_unordered.copy()
-		ssd_bones = ssd_bones_unordered.copy()
-		
-		for i, gt_vals in enumerate(gt_bones):
-			ssd_dist = inf
-			ssd_idx = -1
-			for j in range(i, N):
-				if( np.linalg.norm( gt_vals - ssd_bones_unordered[j] ) < ssd_dist ):
-					ssd_idx = j
-					ssd_dist = np.linalg.norm( gt_vals - ssd_bones_unordered[j] )
-		
-			ssd_bones[i] = ssd_bones_unordered[ssd_idx]
-			ssd_bones_unordered[ssd_idx] = ssd_bones_unordered[i]
-		
-			ssd_w[i] = ssd_w_unordered[ssd_idx]
-			ssd_w_unordered[ssd_idx] = ssd_w_unordered[i]
-			
 		ssd_bones = np.swapaxes(ssd_bones, 0, 1)
 		ssd_vs = np.array([lbs_all(rest_vs, ssd_bones_per_pose, ssd_w.T) for ssd_bones_per_pose in ssd_bones ])
+	
+	if args.debug:
+		np.set_printoptions(threshold=np.nan)
+		print( "############################################" )
+		print( "Per-bone transformation P-by-B-by-12:" )
+		print( rev_bones )
+		print( "############################################" )
+		print( "weights N-by-B:" )
+		print( rev_w.T )
+	
 			
 	print( "############################################" )
 	print( "Per-bone transformation Error: " )
@@ -246,9 +275,7 @@ if __name__ == '__main__':
 			for i, vs in enumerate(ssd_vs):
 				output_path = os.path.join(ssd_folder, str(i+1) + ".obj")
 				format_loader.write_OBJ( output_path, vs.round(6), rest_fs )
-	
-
-		
+			
 # 	plot(gt_bones, ssd_bones, rev_bones )	
  
 	
