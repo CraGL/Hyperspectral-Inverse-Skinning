5297abb193281f25cd5c9f3dadd1549568537610
Songrun <songruner@gmail.com>
2018-01-09 20:50:39 -0500

    recover_poses compared with SSD and ground truth vertex positions.

--- recover_poses.py
+++ recover_poses.py
@@ -45,7 +45,9 @@ if __name__ == '__main__':
 	
 	parser = argparse.ArgumentParser( description='Compare our results and SSD approch results with ground truth.' )
 	parser.add_argument( 'rest_pose', type=str, help='Rest pose (OBJ).')
+	parser.add_argument( 'pose_folder', type=str, help='Folder containing deformed poses.')
 	parser.add_argument( 'result', type=str, help='our results(txt).')
+	parser.add_argument( '--ssd_result', '--SSD', type=str, help='SSD results(txt).')
 	parser.add_argument( '--output', '-O', type=str, help='path to save recovered poses.')
 	args = parser.parse_args()
 		
@@ -70,7 +72,27 @@ if __name__ == '__main__':
 	for i, vs in enumerate(rev_vs):
 		output_path = os.path.join(output_folder, str(i+1) + ".obj")
 		format_loader.write_OBJ( output_path, vs.round(6), rest_fs )
+	
+	gt_mesh_paths = glob.glob(args.pose_folder + "/*.obj")
+	gt_mesh_paths.sort()
+	gt_vs = np.array([ TriMesh.FromOBJ_FileName(mesh_path).vs for mesh_path in gt_mesh_paths ])
+	
+	## diagonal
+	diag = rest_vs.max( axis = 0 ) - rest_vs.min( axis = 0 )
+	diag = np.linalg.norm(diag)
+		
+	print( "############################################" )
+	print( "Reconstruction Mesh Error: " )
+	print( "rev error: ", np.linalg.norm(gt_vs - rev_vs)/(diag*gt_vs.size) )
+	if args.ssd_result is not None:
+		ssd_bones, ssd_w = format_loader.load_result(args.ssd_result)
+		ssd_bones = np.swapaxes(ssd_bones, 0, 1)
+		ssd_vs = np.array([lbs_all(rest_vs, ssd_bones_per_pose, ssd_w.T) for ssd_bones_per_pose in ssd_bones ])
+		print( "ssd error: ", np.linalg.norm(gt_vs - ssd_vs)/(diag*gt_vs.size) )
+	print( "############################################" )
 
+
+	
  
 	
 	
--- results_songrun/cat_pose_no_flat/run.sh
+++ results_songrun/cat_pose_no_flat/run.sh
@@ -13,11 +13,11 @@ SIMPLEX_HULL_ARGS="-D 9"
 
 # Generate
 mkdir -p "${OUTPUT_DIR}"
-# python -u "${ROOT_DIR}"/PerVertex/local_subspace_recover.py ${INITIAL_GUESS_ARGS} "${REST_POSE}" "${OTHER_POSE_DIR}"/*.obj -o "${OUTPUT_DIR}"/local_subspace_recover.txt 2>&1 | tee "${OUTPUT_DIR}"/local_subspace_recover.out
-# python3 -u "${ROOT_DIR}"/simplex_hull_with_initial_guess.py "${OUTPUT_DIR}"/local_subspace_recover.txt "${REST_POSE}" "${OTHER_POSE_DIR}" "${OUTPUT_DIR}"/result.txt ${SIMPLEX_HULL_ARGS} 2>&1 | tee "${OUTPUT_DIR}"/simplex_hull.out
+python -u "${ROOT_DIR}"/PerVertex/local_subspace_recover.py ${INITIAL_GUESS_ARGS} "${REST_POSE}" "${OTHER_POSE_DIR}"/*.obj -o "${OUTPUT_DIR}"/local_subspace_recover.txt 2>&1 | tee "${OUTPUT_DIR}"/local_subspace_recover.out
+python3 -u "${ROOT_DIR}"/simplex_hull_with_initial_guess.py "${OUTPUT_DIR}"/local_subspace_recover.txt "${REST_POSE}" "${OTHER_POSE_DIR}" "${OUTPUT_DIR}"/result.txt ${SIMPLEX_HULL_ARGS} 2>&1 | tee "${OUTPUT_DIR}"/simplex_hull.out
 
 # Evaluate
-python -u "${ROOT_DIR}"/recover_poses.py "${REST_POSE}" "${OUTPUT_DIR}"/result.txt 2>&1 | tee "${OUTPUT_DIR}"/compare.out
+python -u "${ROOT_DIR}"/recover_poses.py "${REST_POSE}" "${OTHER_POSE_DIR}" "${OUTPUT_DIR}"/result.txt --SSD "${SSD_RESULT}" 2>&1 | tee "${OUTPUT_DIR}"/compare.out
 
 
 # Verify each step
