9f4628b2a70b11ffe43dbc7dad21c96d974fd969
Yotam Gingold <yotam@yotamgingold.com>
2018-01-14 02:34:07 -0500

    results_yotam/cheburashka_pose2_no_flat_qp_major/run.sh computes the initial guess again.

--- results_yotam/cheburashka_pose2_no_flat_qp_major/run.sh
+++ results_yotam/cheburashka_pose2_no_flat_qp_major/run.sh
@@ -13,7 +13,7 @@ SIMPLEX_HULL_ARGS="-D 10 --method qp-major --max-iter 20"
 
 # Generate
 mkdir -p "${OUTPUT_DIR}"
-#python -u "${ROOT_DIR}"/PerVertex/local_subspace_recover.py ${INITIAL_GUESS_ARGS} "${REST_POSE}" "${OTHER_POSE_DIR}"/*.obj -o "${OUTPUT_DIR}"/local_subspace_recover.txt 2>&1 | tee "${OUTPUT_DIR}"/local_subspace_recover.out
+python -u "${ROOT_DIR}"/PerVertex/local_subspace_recover.py ${INITIAL_GUESS_ARGS} "${REST_POSE}" "${OTHER_POSE_DIR}"/*.obj -o "${OUTPUT_DIR}"/local_subspace_recover.txt 2>&1 | tee "${OUTPUT_DIR}"/local_subspace_recover.out
 python3 -u "${ROOT_DIR}"/simplex_hull_with_initial_guess.py "${OUTPUT_DIR}"/local_subspace_recover.txt "${REST_POSE}" "${OTHER_POSE_DIR}" "${OUTPUT_DIR}"/result.txt ${SIMPLEX_HULL_ARGS} 2>&1 | tee -i "${OUTPUT_DIR}"/simplex_hull.out
 
 # Evaluate
