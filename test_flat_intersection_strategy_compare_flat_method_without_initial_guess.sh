3d2ac5f54e6cbaa984c78355f9b3e0f3522d3488
jianchao <tanjianchaoustc@gmail.com>
2018-04-18 00:31:40 -0400

    modify script

--- test_flat_intersection_strategy_compare_flat_method_without_initial_guess.sh
+++ test_flat_intersection_strategy_compare_flat_method_without_initial_guess.sh
@@ -24,24 +24,24 @@ do
 	H=${test_dir#$name-}
 	for SEED in `seq 0 0`
 	do
-		# ## biquadratic
-		# FLAT_INTERSECTION_ARGS_BIQUADRATIC="--energy biquadratic --forced-init True --seed $SEED --CSV ${OUTPUT_DIR}/${test_dir}_biquadratic_${SEED}.csv --strategy pinv+ssv:weighted --max-iter ${MAXITER} --f-eps 0 --handles ${H}"
-		# python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_BIQUADRATIC} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_biquadratic.out
+		## biquadratic
+		FLAT_INTERSECTION_ARGS_BIQUADRATIC="--energy biquadratic --forced-init True --seed $SEED --CSV ${OUTPUT_DIR}/${test_dir}_biquadratic_${SEED}.csv --strategy pinv+ssv:weighted --max-iter ${MAXITER} --f-eps 0 --handles ${H}"
+		python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_BIQUADRATIC} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_biquadratic.out
 
-		# ## B
-		# FLAT_INTERSECTION_ARGS_B="--energy B --forced-init True --seed $SEED --CSV ${OUTPUT_DIR}/${test_dir}_b_${SEED}.csv --W-projection normalize --max-iter ${MAXITER} --f-eps 0 --handles ${H}"
-		# python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_B} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_b.out
+		## B
+		FLAT_INTERSECTION_ARGS_B="--energy B --forced-init True --seed $SEED --CSV ${OUTPUT_DIR}/${test_dir}_b_${SEED}.csv --W-projection normalize --max-iter ${MAXITER} --f-eps 0 --handles ${H}"
+		python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_B} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_b.out
  				
-		# ## IPCA 
-		# FLAT_INTERSECTION_ARGS_IPCA="--energy ipca --seed $SEED --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_ipca_${SEED}.csv --W-projection normalize --max-iter ${MAXITER} --f-eps 0 --handles ${H}"
-		# python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_IPCA} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_ipca.out	
+		## IPCA 
+		FLAT_INTERSECTION_ARGS_IPCA="--energy ipca --seed $SEED --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_ipca_${SEED}.csv --W-projection normalize --max-iter ${MAXITER} --f-eps 0 --handles ${H}"
+		python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_IPCA} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_ipca.out	
 
         # # ## pymanopt pB
         FLAT_INTERSECTION_ARGS_BIQUADRATIC="--energy B --forced-init True --seed $SEED --max-iter 0 --f-eps 0 --handles ${H}"
         python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_BIQUADRATIC} --save-matlab-initial "${OUTPUT_DIR}"/filename.mat
         x=$((H-1))
         echo $x
-        maxiter=$((MAXITER+1))
+        maxiter=$((MAXITER+1)) ### seems above three methods are 21 iterations actually.
         echo $maxiter
         python3 -u pymanopt_test_karcher.py --load "${OUTPUT_DIR}"/filename.mat --test-data "${OUTPUT_DIR}"/filename.mat --handles $x --optimize-solver conjugate --manifold pB  --max-iter $maxiter  2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_pymanopt_pB.out
 
