5143a28ba49d610db6fe783249b78fb62b39abe0
jianchao <tanjianchaoustc@gmail.com>
2018-04-23 23:45:52 -0400

    update script to plot new figures

--- plot_strategy.py
+++ plot_strategy.py
@@ -26,7 +26,7 @@ def plot(all_data):
 		y = item[1]
 		
 
-		plt.title('Different optimization strategies with performance' , fontsize='medium')
+		# plt.title('Different optimization strategies with performance' , fontsize='medium')
 		plt.xlabel('iterations')
 		plt.ylabel('Vertex Error')
 
@@ -65,42 +65,70 @@ def plot(all_data):
 
 
 
-############ with initial guess. So laplacian choice is included.
-# ## cylinder
-# 		plt.plot(range(len(y[0])), y[0], c=cmap[0], lw=2, label='Biquadratic (0.08 min)')
-# 		plt.plot(range(len(y[1])), y[1], c=cmap[1], lw=2, label='pB (0.06 min)')
-# 		plt.plot(range(len(y[2])), y[2], c=cmap[2], lw=2, label='IPCA (0.07 min)')
-# 		plt.plot(range(len(y[3])), y[3], c=cmap[3], lw=2, label='pymanopt_pB_conjugate (0.29 min)')
-# 		plt.plot(range(len(y[4])), y[4], c=cmap[4], lw=2, label='pymanopt_pB_trust (17.41 min)')
-# 		plt.plot(range(len(y[5])), y[5], c=cmap[5], lw=2, label='pymanopt_pB_steepest (0.33 min)')
-# 		plt.plot(range(len(y[6])), y[6], c=cmap[6], lw=2, label='intersection_conjugate (0.33 min)')
-# 		plt.plot(range(len(y[7])), y[7], c=cmap[7], lw=2, label='laplacian (0.34 min)')
-# 		plt.ylim(ymin=0, ymax=370)
+############ with initial guess (50%)
+## cylinder
+		# plt.plot(range(len(y[0])), y[0], c=cmap[0], lw=2, label='Biquadratic (0.08 min)')
+		# plt.plot(range(len(y[1])), y[1], c=cmap[1], lw=2, label='p,B (0.06 min)')
+		# plt.plot(range(len(y[2])), y[2], c=cmap[2], lw=2, label='IPCA (0.06 min)')
+		# plt.plot(range(len(y[3])), y[3], c=cmap[3], lw=2, label='Manifold p,B conjugate (0.31 min)')
+		# plt.plot(range(len(y[4])), y[4], c=cmap[4], lw=2, label='Manifold p,B trust (93.13 min)')
+		# plt.plot(range(len(y[5])), y[5], c=cmap[5], lw=2, label='Manifold p,B steepest (0.26 min)')
+		# plt.ylim(ymin=0, ymax=32)
+		# # plt.plot(range(len(y[6])), y[6], c=cmap[6], lw=2, label='Intersection conjugate (0.31 min)')
+		# # plt.ylim(ymin=0, ymax=140)
 
 
-## cat
-		plt.plot(range(len(y[0])), y[0], c=cmap[0], lw=2, label='Biquadratic (1.91 min)')
-		plt.plot(range(len(y[1])), y[1], c=cmap[1], lw=2, label='pB (1.55 min)')
-		plt.plot(range(len(y[2])), y[2], c=cmap[2], lw=2, label='IPCA (1.90 min)')
-		plt.plot(range(len(y[3])), y[3], c=cmap[3], lw=2, label='pymanopt_pB_conjugate (6.84 min)')
-		plt.plot(range(len(y[4])), y[4], c=cmap[4], lw=2, label='pymanopt_pB_trust (18.87 min)')
-		plt.plot(range(len(y[5])), y[5], c=cmap[5], lw=2, label='pymanopt_pB_steepest (6.64 min)')
-		plt.plot(range(len(y[6])), y[6], c=cmap[6], lw=2, label='intersection_conjugate (17.61 min)')
-		# plt.plot(range(len(y[7])), y[7], c=cmap[7], lw=2, label='laplacian (?? min)')
-		plt.ylim(ymin=0, ymax=45)
+
+  # #       ### log y version
+		# plt.semilogy(range(len(y[0])), y[0], c=cmap[0], lw=2, label='Biquadratic (0.08 min)')
+		# plt.semilogy(range(len(y[1])), y[1], c=cmap[1], lw=2, label='p,B (0.06 min)')
+		# plt.semilogy(range(len(y[2])), y[2], c=cmap[2], lw=2, label='IPCA (0.06 min)')
+		# plt.semilogy(range(len(y[3])), y[3], c=cmap[3], lw=2, label='Manifold p,B conjugate (0.31 min)')
+		# plt.semilogy(range(len(y[4])), y[4], c=cmap[4], lw=2, label='Manifold p,B trust (93.13 min)')
+		# plt.semilogy(range(len(y[5])), y[5], c=cmap[5], lw=2, label='Manifold p,B steepest (0.26 min)')
+		# plt.ylim(ymin=0, ymax=40)
+		# # plt.semilogy(range(len(y[6])), y[6], c=cmap[6], lw=2, label='Intersection conjugate (0.31 min)')
+		# # plt.ylim(ymin=0, ymax=1e7)
+
+		# plt.semilogy(range(len(y[0])), y[0], 'o', c=cmap[0])
+		# plt.semilogy(range(len(y[1])), y[1], 'o', c=cmap[1])
+		# plt.semilogy(range(len(y[2])), y[2], 'o', c=cmap[2])
+		# plt.semilogy(range(len(y[3])), y[3], 'o', c=cmap[3])
+		# plt.semilogy(range(len(y[4])), y[4], 'o', c=cmap[4])
+		# plt.semilogy(range(len(y[5])), y[5], 'o', c=cmap[5])
+		# # plt.semilogy(range(len(y[6])), y[6], 'o', c=cmap[6])
 
 
-# ### cheburashka
-# 		plt.plot(range(len(y[0])), y[0], c=cmap[0], lw=2, label='Biquadratic (1.57 min)')
-# 		plt.plot(range(len(y[1])), y[1], c=cmap[1], lw=2, label='pB (1.20 min)')
-# 		plt.plot(range(len(y[2])), y[2], c=cmap[2], lw=2, label='IPCA (2.15 min)')
-# 		plt.plot(range(len(y[3])), y[3], c=cmap[3], lw=2, label='pymanopt_pB_conjugate (6.00 min)')
-# 		plt.plot(range(len(y[4])), y[4], c=cmap[4], lw=2, label='pymanopt_pB_trust (18.58 min)')
-# 		plt.plot(range(len(y[5])), y[5], c=cmap[5], lw=2, label='pymanopt_pB_steepest (6.29 min)')
-# 		plt.plot(range(len(y[6])), y[6], c=cmap[6], lw=2, label='intersection_conjugate (17.77 min)')
-# 		# plt.plot(range(len(y[7])), y[7], c=cmap[7], lw=2, label='laplacian (?? min)')
-# 		plt.ylim(ymin=0, ymax=30)
+
+# ## cat
+# 		plt.plot(range(len(y[0])), y[0], c=cmap[0], lw=2, label='Biquadratic (1.86 min)')
+# 		plt.plot(range(len(y[1])), y[1], c=cmap[1], lw=2, label='p,B (1.48 min)')
+# 		plt.plot(range(len(y[2])), y[2], c=cmap[2], lw=2, label='IPCA (1.85 min)')
+# 		plt.plot(range(len(y[3])), y[3], c=cmap[3], lw=2, label='Manifold p,B conjugate (6.37 min)')
+# 		plt.plot(range(len(y[4])), y[4], c=cmap[4], lw=2, label='Manifold p,B trust (269.36 min)')
+# 		plt.plot(range(len(y[5])), y[5], c=cmap[5], lw=2, label='Manifold p,B steepest (7.17 min)')
+# 		plt.ylim(ymin=0, ymax=10)
+
+# 		# plt.plot(range(len(y[6])), y[6], c=cmap[6], lw=2, label='Intersection conjugate (18.51 min)')
+# 		# plt.ylim(ymin=0, ymax=16)
+
+
+## cheburashka
+		plt.plot(range(len(y[0])), y[0], c=cmap[0], lw=2, label='Biquadratic (1.47 min)')
+		plt.plot(range(len(y[1])), y[1], c=cmap[1], lw=2, label='p,B (1.38 min)')
+		plt.plot(range(len(y[2])), y[2], c=cmap[2], lw=2, label='IPCA (2.38 min)')
+		plt.plot(range(len(y[3])), y[3], c=cmap[3], lw=2, label='Manifold p,B conjugate (7.04 min)')
+		plt.plot(range(len(y[4])), y[4], c=cmap[4], lw=2, label='Manifold p,B trust (550.98 min)')
+		plt.plot(range(len(y[5])), y[5], c=cmap[5], lw=2, label='Manifold p,B steepest (5.17 min)')
+		plt.ylim(ymin=0, ymax=6)
+
+		# plt.plot(range(len(y[6])), y[6], c=cmap[6], lw=2, label='Intersection conjugate (21.74 min)')
+		# plt.ylim(ymin=0, ymax=25)
 		
+
+
+
+
 
 		plt.plot(range(len(y[0])), y[0], 'o', c=cmap[0])
 		plt.plot(range(len(y[1])), y[1], 'o', c=cmap[1])
@@ -108,12 +136,11 @@ def plot(all_data):
 		plt.plot(range(len(y[3])), y[3], 'o', c=cmap[3])
 		plt.plot(range(len(y[4])), y[4], 'o', c=cmap[4])
 		plt.plot(range(len(y[5])), y[5], 'o', c=cmap[5])
-		plt.plot(range(len(y[6])), y[6], 'o', c=cmap[6])
-		# plt.plot(range(len(y[7])), y[7], 'o', c=cmap[7])
+		# plt.plot(range(len(y[6])), y[6], 'o', c=cmap[6])
 
 
-		
-		plt.legend( title=name+' with '+handles+' handles'+ ' and 0 as seed', loc='upper right', bbox_to_anchor=(1,1), ncol=1, fontsize='medium')
+		plt.legend( title=name+' with '+handles+' handles', loc='upper right', fontsize='small')
+		# plt.legend( title=name+' with '+handles+' handles', loc='upper right', bbox_to_anchor=(1,1), ncol=1, fontsize='medium')
 		plt.savefig('strategy_comparison_'+ item[0] +'.pdf', bbox_inches='tight')
 		plt.show()
 
@@ -131,19 +158,16 @@ if __name__ == '__main__':
 	for folder in args.example:
 		name = folder.split(os.sep)[-1]
 		if name == '':	name = folder.split(os.sep)[-2]
-		data_file_biquadratic = os.path.join( folder, name+'_biquadratic_0.csv' )
-		data_file_b = os.path.join( folder, name+'_b_0.csv' )
-		data_file_ipca = os.path.join( folder, name+'_ipca_0.csv' )
-		data_file_pB_pymanopt_conjugate = os.path.join( folder, name+'_pB_pymanopt_conjugate_0.csv' )
-		data_file_pB_pymanopt_trust = os.path.join( folder, name+'_pB_pymanopt_trust_0.csv' )
-		data_file_pB_pymanopt_steepest = os.path.join( folder, name+'_pB_pymanopt_steepest_0.csv' )
-		data_file_intersection_conjugate = os.path.join( folder, name+'_intersection_conjugate_0.csv' )
-		data_file_laplacian = os.path.join( folder, name+'_laplacian_0.csv' )
+		data_file_biquadratic = os.path.join( folder, name+'_biquadratic.csv' )
+		data_file_b = os.path.join( folder, name+'_b.csv' )
+		data_file_ipca = os.path.join( folder, name+'_ipca.csv' )
+		data_file_pB_pymanopt_conjugate = os.path.join( folder, name+'_pB_pymanopt_conjugate.csv' )
+		data_file_pB_pymanopt_trust = os.path.join( folder, name+'_pB_pymanopt_trust-manually_corrected.csv' )
+		data_file_pB_pymanopt_steepest = os.path.join( folder, name+'_pB_pymanopt_steepest.csv' )
+		data_file_intersection_conjugate = os.path.join( folder, name+'_intersection_conjugate.csv' )
 
 
 		all_data[name] = [np.loadtxt(data_file_biquadratic, delimiter=',')[:20], np.loadtxt(data_file_b, delimiter=',')[:20], np.loadtxt(data_file_ipca, delimiter=',')[:20], np.loadtxt(data_file_pB_pymanopt_conjugate, delimiter=',')[:20], np.loadtxt(data_file_pB_pymanopt_trust, delimiter=',')[:20], np.loadtxt(data_file_pB_pymanopt_steepest, delimiter=',')[:20], np.loadtxt(data_file_intersection_conjugate, delimiter=',')[:20]]
-
-		# all_data[name] = [np.loadtxt(data_file_biquadratic, delimiter=',')[:20], np.loadtxt(data_file_b, delimiter=',')[:20], np.loadtxt(data_file_ipca, delimiter=',')[:20], np.loadtxt(data_file_pB_pymanopt_conjugate, delimiter=',')[:20], np.loadtxt(data_file_pB_pymanopt_trust, delimiter=',')[:20], np.loadtxt(data_file_pB_pymanopt_steepest, delimiter=',')[:20], np.loadtxt(data_file_intersection_conjugate, delimiter=',')[:20], np.loadtxt(data_file_laplacian, delimiter=',')[:20] ]
 	
 	plot(all_data)
 
--- test_flat_intersection_strategy_compare_flat_method_with_initial_guess.sh
+++ test_flat_intersection_strategy_compare_flat_method_with_initial_guess.sh
@@ -6,15 +6,13 @@ RES_DIR="./results_strategy_compare_flat_method_with_initial_guess"
 OBJ_SUFF=".obj"
 MAXITER=20
 
-INITIAL_GUESS_ARGS="--svd_threshold 1e-15 --transformation_percentile 50 --version 0 --method vertex -rand none"
-
 
 POSES_DIR="${MODEL_DIR}/${name}"
 # for OUTPUT_DIR in "${RES_DIR}"/cat-poses*  "${RES_DIR}"/elephant-gallop* "${RES_DIR}"/horse-collapse*  "${RES_DIR}"/chickenCrossing*  "${RES_DIR}"/pdance*
 # for OUTPUT_DIR in "${RES_DIR}"/horse-collapse*  "${RES_DIR}"/chickenCrossing*  "${RES_DIR}"/pdance*
 
 # for OUTPUT_DIR in "${RES_DIR}"/cylinder*  "${RES_DIR}"/cube* "${RES_DIR}"/cheburashka*  "${RES_DIR}"/wolf*  "${RES_DIR}"/cow*
-# for OUTPUT_DIR in "${RES_DIR}"/cheburashka* "${RES_DIR}"/cat-poses* 
+# for OUTPUT_DIR in "${RES_DIR}"/cylinder* "${RES_DIR}"/cheburashka* "${RES_DIR}"/cat-poses* 
 for OUTPUT_DIR in "${RES_DIR}"/cylinder*
 # for OUTPUT_DIR in "${RES_DIR}"/cheburashka* 
 do
@@ -26,40 +24,48 @@ do
 	POSES_DIR="${MODEL_DIR}"/"${name}"
 	H=${test_dir#$name-}
 
-    #### if print-all is True, then all qs_data are used for flat_intersection.py
-	python -u PerVertex/local_subspace_recover.py ${INITIAL_GUESS_ARGS} "${REST_POSE}" "${POSES_DIR}"/*.obj --print-all True --out "${OUTPUT_DIR}"/local_subspace_recover.txt --out-errors "${OUTPUT_DIR}"/local_subspace_recover_errors.txt --out-ssv "${OUTPUT_DIR}"/local_subspace_recover_ssv.txt  2>&1 | tee "${OUTPUT_DIR}"/local_subspace_recover.out
 
-	## biquadratic
-	FLAT_INTERSECTION_ARGS_BIQUADRATIC="--energy biquadratic --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_biquadratic.csv --strategy pinv+ssv:weighted --max-iter ${MAXITER} --f-eps 0 --handles ${H} -I ${OUTPUT_DIR}/local_subspace_recover.txt"
-	python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_BIQUADRATIC} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_biquadratic.out
+ #    ### initial guess: one ring, vertex method, unconstrained, 50% data saved
+	# INITIAL_GUESS_ARGS="--svd_threshold 1e-15 --transformation_percentile 50 --version 0 --method vertex -rand none"
+	# python -u PerVertex/local_subspace_recover.py ${INITIAL_GUESS_ARGS} "${REST_POSE}" "${POSES_DIR}"/*.obj --out "${OUTPUT_DIR}"/local_subspace_recover.txt  2>&1 | tee "${OUTPUT_DIR}"/local_subspace_recover.out
 
-	## B
-	FLAT_INTERSECTION_ARGS_B="--energy B --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_b.csv --W-projection normalize --max-iter ${MAXITER} --f-eps 0 --handles ${H} -I ${OUTPUT_DIR}/local_subspace_recover.txt"
-	python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_B} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_b.out
+	# ## biquadratic
+	# FLAT_INTERSECTION_ARGS_BIQUADRATIC="--energy biquadratic --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_biquadratic.csv --strategy pinv+ssv:weighted --max-iter ${MAXITER} --f-eps 0 --handles ${H} -I ${OUTPUT_DIR}/local_subspace_recover.txt"
+	# python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_BIQUADRATIC} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_biquadratic.out
 
-	## IPCA 
-	FLAT_INTERSECTION_ARGS_IPCA="--energy ipca --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_ipca.csv --W-projection normalize --max-iter ${MAXITER} --f-eps 0 --handles ${H} -I ${OUTPUT_DIR}/local_subspace_recover.txt"
-	python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_IPCA} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_ipca.out	
+	# ## B
+	# FLAT_INTERSECTION_ARGS_B="--energy B --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_b.csv --W-projection normalize --max-iter ${MAXITER} --f-eps 0 --handles ${H} -I ${OUTPUT_DIR}/local_subspace_recover.txt"
+	# python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_B} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_b.out
 
-	## pb_pymanopt conjugate
-	FLAT_INTERSECTION_ARGS_PB_PYMANOPT_CONJUGATE="--energy pB_pymanopt --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_pB_pymanopt_conjugate.csv --strategy conjugate --max-iter ${MAXITER} --f-eps 0 --handles ${H} -I ${OUTPUT_DIR}/local_subspace_recover.txt"
-	python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_PB_PYMANOPT_CONJUGATE} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_pB_pymanopt_conjugate.out
+	# ## IPCA 
+	# FLAT_INTERSECTION_ARGS_IPCA="--energy ipca --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_ipca.csv --W-projection normalize --max-iter ${MAXITER} --f-eps 0 --handles ${H} -I ${OUTPUT_DIR}/local_subspace_recover.txt"
+	# python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_IPCA} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_ipca.out	
 
-	## pb_pymanopt trust
-	FLAT_INTERSECTION_ARGS_PB_PYMANOPT_TRUST="--energy pB_pymanopt --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_pB_pymanopt_trust.csv --strategy trust --max-iter ${MAXITER} --f-eps 0 --handles ${H} -I ${OUTPUT_DIR}/local_subspace_recover.txt"
-	python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_PB_PYMANOPT_TRUST} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_pB_pymanopt_trust.out
+	# ## pb_pymanopt conjugate
+	# FLAT_INTERSECTION_ARGS_PB_PYMANOPT_CONJUGATE="--energy pB_pymanopt --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_pB_pymanopt_conjugate.csv --strategy conjugate --max-iter ${MAXITER} --f-eps 0 --handles ${H} -I ${OUTPUT_DIR}/local_subspace_recover.txt"
+	# python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_PB_PYMANOPT_CONJUGATE} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_pB_pymanopt_conjugate.out
 
-	## pb_pymanopt steepest
-	FLAT_INTERSECTION_ARGS_PB_PYMANOPT_STEEPEST="--energy pB_pymanopt --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_pB_pymanopt_steepest.csv --strategy steepest --max-iter ${MAXITER} --f-eps 0 --handles ${H} -I ${OUTPUT_DIR}/local_subspace_recover.txt"
-	python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_PB_PYMANOPT_STEEPEST} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_pB_pymanopt_steepest.out
+	# ## pb_pymanopt trust
+	# FLAT_INTERSECTION_ARGS_PB_PYMANOPT_TRUST="--energy pB_pymanopt --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_pB_pymanopt_trust.csv --strategy trust --max-iter ${MAXITER} --f-eps 0 --handles ${H} -I ${OUTPUT_DIR}/local_subspace_recover.txt"
+	# python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_PB_PYMANOPT_TRUST} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_pB_pymanopt_trust.out
 
-	## intersection conjugate
-	FLAT_INTERSECTION_ARGS_INTERSECTION="--energy intersection --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_intersection_conjugate.csv --strategy conjugate --max-iter ${MAXITER} --f-eps 0 --handles ${H} -I ${OUTPUT_DIR}/local_subspace_recover.txt"
-	python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_INTERSECTION} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_intersection_conjugate.out
+	# ## pb_pymanopt steepest
+	# FLAT_INTERSECTION_ARGS_PB_PYMANOPT_STEEPEST="--energy pB_pymanopt --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_pB_pymanopt_steepest.csv --strategy steepest --max-iter ${MAXITER} --f-eps 0 --handles ${H} -I ${OUTPUT_DIR}/local_subspace_recover.txt"
+	# python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_PB_PYMANOPT_STEEPEST} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_pB_pymanopt_steepest.out
 
-	# ## laplacian
-	FLAT_INTERSECTION_ARGS_LAPLACIAN="--energy laplacian --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_laplacian.csv --max-iter ${MAXITER} --f-eps 0 --handles ${H} --fancy-init ${OUTPUT_DIR}/local_subspace_recover.txt --fancy-init-errors ${OUTPUT_DIR}/local_subspace_recover_errors.txt --fancy-init-ssv ${OUTPUT_DIR}/local_subspace_recover_ssv.txt"
-	python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_LAPLACIAN} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_laplacian.out
+	# ## intersection conjugate
+	# FLAT_INTERSECTION_ARGS_INTERSECTION="--energy intersection --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_intersection_conjugate.csv --strategy conjugate --max-iter ${MAXITER} --f-eps 0 --handles ${H} -I ${OUTPUT_DIR}/local_subspace_recover.txt"
+	# python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_INTERSECTION} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_intersection_conjugate.out
+
+
+
+
+ # #    #### not used anymore
+ # #    #### if print-all is True, then all qs_data are used for flat_intersection.py
+	# # python -u PerVertex/local_subspace_recover.py ${INITIAL_GUESS_ARGS} "${REST_POSE}" "${POSES_DIR}"/*.obj --print-all True --out "${OUTPUT_DIR}"/local_subspace_recover_100percent.txt --out-errors "${OUTPUT_DIR}"/local_subspace_recover_errors_100percent.txt --out-ssv "${OUTPUT_DIR}"/local_subspace_recover_ssv_100percent.txt  2>&1 | tee "${OUTPUT_DIR}"/local_subspace_recover_100percent.out
+	# # # ## laplacian
+	# # FLAT_INTERSECTION_ARGS_LAPLACIAN="--energy laplacian --forced-init True --CSV ${OUTPUT_DIR}/${test_dir}_laplacian.csv --max-iter ${MAXITER} --f-eps 0 --handles ${H} --fancy-init ${OUTPUT_DIR}/local_subspace_recover_100percent.txt --fancy-init-errors ${OUTPUT_DIR}/local_subspace_recover_errors_100percent.txt --fancy-init-ssv ${OUTPUT_DIR}/local_subspace_recover_ssv_100percent.txt"
+	# # python3 -u flat_intersection.py "${REST_POSE}" "${POSES_DIR}" ${FLAT_INTERSECTION_ARGS_LAPLACIAN} 2>&1 | tee -i "${OUTPUT_DIR}"/flat_intersection_laplacian_100percent.out
 
 echo
 done	
