1b6f06bc296702ed7297ce2f03d676a7f437c554
Yotam Gingold <yotam@yotamgingold.com>
2019-11-24 22:23:48 -0500

    added missing script to run animation comparison

---
+++ test_guoliang.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"crane.obj "../for Guoliang/"crane "../for Guoliang reply/"carne_decode_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"chickenCrossing.obj "../for Guoliang/"chickenCrossing "../for Guoliang reply/"chickencrossing_decode_4.1346_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"chickenCrossing.obj "../for Guoliang/"chickenCrossing "../for Guoliang reply/"chickencrossing_decode_5.7885_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"elasticCow.obj "../for Guoliang/"elasticCow "../for Guoliang reply/"elasticCow_decode_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"elephant-gallop.obj "../for Guoliang/"elephant-gallop "../for Guoliang reply/"elephant-gallop_decode_13.5148_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"elephant-gallop.obj "../for Guoliang/"elephant-gallop "../for Guoliang reply/"elephant-gallop_decode_18.2449_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"elephant-gallop.obj "../for Guoliang/"elephant-gallop "../for Guoliang reply/"elephant-gallop_decode_6.7574_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"elephant.obj "../for Guoliang/"elephant "../for Guoliang reply/"elephant_decode_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"horse-collapse.obj "../for Guoliang/"horse-collapse "../for Guoliang reply/"horse-collapse_decode_12.9863_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"horse-collapse.obj "../for Guoliang/"horse-collapse "../for Guoliang reply/"horse-collapse_decode_6.4931_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"horse-gallop.obj "../for Guoliang/"horse-gallop "../for Guoliang reply/"horse-gallop_decode_14.2442_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"horse-gallop.obj "../for Guoliang/"horse-gallop "../for Guoliang reply/"horse-gallop_decode_23.5030_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"horse-gallop.obj "../for Guoliang/"horse-gallop "../for Guoliang reply/"horse-gallop_decode_7.1221_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"horse.obj "../for Guoliang/"horse "../for Guoliang reply/"horse_decode_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"pcow.obj "../for Guoliang/"pcow "../for Guoliang reply/"pcow_decode_2.8909_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"pcow.obj "../for Guoliang/"pcow "../for Guoliang reply/"pcow_decode_6.9382_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"pdance.obj "../for Guoliang/"pdance "../for Guoliang reply/"pdance_decode_2.1358_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"pdance.obj "../for Guoliang/"pdance "../for Guoliang reply/"pdance_decode_5.1260_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"pjump.obj "../for Guoliang/"pjump "../for Guoliang reply/"pjump_decode_3.3680_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"pjump.obj "../for Guoliang/"pjump "../for Guoliang reply/"pjump_decode_6.7360_seq/ 2>&1
+python -u compare_E_RMS_kavan2010.py "../for Guoliang/"samba.obj "../for Guoliang/"samba "../for Guoliang reply/"samba_decode_seq/ 2>&1
